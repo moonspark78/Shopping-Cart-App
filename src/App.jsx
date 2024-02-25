43300d63be42de77c8680f7d2aa1cba8c67da008
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-25 23:48:41 +0100

    done with  navbar :white_check_mark:

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import {Home } from "./pages/Home"
 import {Cart } from "./pages/Cart"
+import {Navbar} from "./components/Navbar"
 
 function App() {
   return (
     <div>
-      <h1>Navbar</h1>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/cart' element={<Cart/>}/>
