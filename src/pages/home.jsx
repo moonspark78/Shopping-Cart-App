a6ee1a4ee9f9ab5c657dff98d376eac15d0cc300
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-26 00:17:26 +0100

    start with the home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Home = () => {
+
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(()=> {
+    const fetchData = async () => {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
+        console.log(data);
+        if (data){
+          setLoading(false);
+          setProducts(data);
+        }
+    };
+    fetchData();
+  },[])
+
+
+
+
   return (
     <div>Home</div>
   )
