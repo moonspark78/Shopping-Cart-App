61452b5ad284f37812eed4c49a62c46fb6a5a737
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-27 20:31:02 +0100

    starting with redux

---
+++ src/store/slices/cart-slice.js
@@ -0,0 +1,12 @@
+// import createslice
+// initialize the initial state
+// create the createslice and give it a name, mention the initial state, and actions
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+});
