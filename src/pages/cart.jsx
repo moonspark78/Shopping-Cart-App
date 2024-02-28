7c6c22d9d2cde21f01203991fe316936a219a74b
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-28 19:39:19 +0100

    working on the ui of cart Component

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,16 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export const Cart = () => {
   const [totalCart, setTotalCart] = useState(0);
 
   const { cart } = useSelector((state) => state);
 
-  useEffect(() =>{
-    setTotalCart(cart.reduce((acc,cur) => acc + cur.price, 0));
-  },[cart])
+  useEffect(() => {
+    setTotalCart(cart.reduce((acc, cur) => acc + cur.price, 0));
+  }, [cart]);
 
   console.log(cart, totalCart);
 
-  return <div>Cart</div>;
+  return (
+    <div>
+      {cart && cart.length ? (
+        <div></div>
+      ) : (
+        <div className="min-h-[80vh] flex flex-col items-center justify-center">
+          <h1 className="text-gray-800 font-bold text-xl mb-2">
+            Your Cart is empty
+          </h1>
+          <Link to="/">
+            <button className="bg-red-950 text-white border-2 rounded-lg font-bold p-4">
+              SHOP NOW
+            </button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 };
