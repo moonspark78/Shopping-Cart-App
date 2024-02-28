0a8dec179e92a70a8ef757ead2223c647b41c1e9
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-28 18:21:10 +0100

    working on redux side

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 export const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const [totalCart, setTotalCart] = useState(0);
+
+  const { cart } = useSelector((state) => state);
+
+  useEffect(() =>{
+    setTotalCart(cart.reduce((acc,cur) => acc + cur.price, 0));
+  },[cart])
+
+  console.log(cart, totalCart);
+
+  return <div>Cart</div>;
+};
