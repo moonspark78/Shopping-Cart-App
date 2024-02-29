ad48fa53c094148b086e77863c59164a0725d2d2
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-29 19:12:21 +0100

    working on the ui of cart Component pt.2

---
+++ src/components/CartTile.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CartTile = ({cartItem}) => {
+  return (
+    <div>CartTile</div>
+  )
+}
--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { CartTile } from "../components/CartTile";
 
 export const Cart = () => {
   const [totalCart, setTotalCart] = useState(0);
@@ -16,7 +17,20 @@ export const Cart = () => {
   return (
     <div>
       {cart && cart.length ? (
-        <div></div>
+        <>
+          <div className="min-h-[80vh] grid md:grid-cols-2 max-w-6xl mx-auto">
+            <div className="flex flex-col justify-center items-center p-3">
+              {cart.map((cartItem) => (
+                <CartTile cartItem={cartItem} />
+              ))}
+            </div>
+          </div>
+          <div>
+            <div>
+              
+            </div>
+          </div>
+        </>
       ) : (
         <div className="min-h-[80vh] flex flex-col items-center justify-center">
           <h1 className="text-gray-800 font-bold text-xl mb-2">
